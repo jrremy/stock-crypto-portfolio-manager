a02aea6def6978c71afcd10297c3fb31ae6fbf05
Jeremy <jeremyd1818@gmail.com>
2025-02-02 02:32:09 -0500

    rm sb

--- portfolio-manager/src/main/java/com/jrremy/portfolio_manager/portfolio/repository/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jrremy.portfolio_manager.portfolio.repository;
-
-import com.jrremy.portfolio_manager.portfolio.model.Transaction;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionRepository extends CrudRepository<Transaction, Long> {
-}
--- portfolio-manager/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE transactions (
-    id SERIAL PRIMARY KEY,
-    asset_type VARCHAR(10) NOT NULL,      -- "crypto" or "stock"
-    ticker VARCHAR(10) NOT NULL,         -- e.g., BTC, AAPL
-    transaction_type VARCHAR(10) NOT NULL, -- "buy", "sell", "swap"
-    quantity NUMERIC(10, 2) NOT NULL,
-    price NUMERIC(10, 2) NOT NULL,
-    timestamp TIMESTAMP NOT NULL
-);
